2db43f5078a0cf188866ac9443acd7b305c524e9
AndreVsc <95365578+AndreVsc@users.noreply.github.com>
2024-08-19 13:53:11 -0300

    add challenge 04

--- src/exercices/challenge04/App.java
+++ src/exercices/challenge04/App.java
@@ -2,13 +2,21 @@ package exercices.challenge04;
 
 import java.util.Arrays;
 import java.util.List;
-
-// Utilize a Stream API para remover os valores ímpares da lista e imprima a lista resultante no console.
+import java.util.stream.Collectors;
 
 public class App {
     List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-    void removeImpares(){
-        
+    void removeImpares() {
+
+        List<Integer> numerosPares = numeros.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
+
+        System.out.println("Lista após remover os ímpares: " + numerosPares);
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+
+        app.removeImpares();
     }
 }
