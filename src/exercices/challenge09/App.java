800183989c62fb83589a03e7f3c0410af1ca9357
AndreVsc <95365578+AndreVsc@users.noreply.github.com>
2024-08-19 14:50:25 -0300

    add challenge 09

--- src/exercices/challenge09/App.java
+++ src/exercices/challenge09/App.java
@@ -2,11 +2,24 @@ package exercices.challenge09;
 
 import java.util.List;
 import java.util.Arrays;
-
-// Com a Stream API, verifique se todos os números da lista são distintos (não se repetem) e exiba o resultado no console.
+import java.util.HashSet;
+import java.util.Set;
 
 public class App {
 
     List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-}   
+    void isDifferent() {
+        Set<Integer> numerosUnicos = new HashSet<>(numeros);
+        
+        boolean todosDistintos = numerosUnicos.size() == numeros.size();
+        
+        System.out.println("All numbers is distincts? : " + todosDistintos);
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+
+        app.isDifferent();
+    }
+}
