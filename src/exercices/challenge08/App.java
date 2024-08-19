717dfdc0e09bc215e99120b1797aae3172f90e42
AndreVsc <95365578+AndreVsc@users.noreply.github.com>
2024-08-19 14:45:40 -0300

    add challenge 08

--- src/exercices/challenge08/App.java
+++ src/exercices/challenge08/App.java
@@ -1,5 +1,20 @@
 package exercices.challenge08;
 
+import java.util.List;
+import java.util.Arrays;
+
 public class App {
-    
+
+    List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+    void calculedAllNumbers() {
+        int somaDosDigitos = numeros.stream().flatMapToInt(num -> Integer.toString(num).chars().map(c -> c - '0')).sum();
+        System.out.println("A soma dos dígitos de todos os números é: " + somaDosDigitos);
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        
+        app.calculedAllNumbers();
+    }
 }
--- src/exercices/challenge09/App.java
+++ src/exercices/challenge09/App.java
@@ -1,5 +1,12 @@
 package exercices.challenge09;
 
+import java.util.List;
+import java.util.Arrays;
+
+// Com a Stream API, verifique se todos os números da lista são distintos (não se repetem) e exiba o resultado no console.
+
 public class App {
-    
-}
+
+    List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+}   
